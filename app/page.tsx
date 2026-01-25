62b5f80238ab7fe36228a2b8ddea7c06089b1ac7
ruspian <ruspianntb@gmail.com>
2026-01-26 01:21:03 +0800

    tambahkan suspen untuk cover

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Cover from "@/components/Cover";
 import MainContent from "@/components/MainContent";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="relative min-h-screen">
-      <Cover />
+      <Suspense fallback={<div className="fixed inset-0 bg-[#F4F7F5]" />}>
+        <Cover />
+      </Suspense>
 
       <MainContent />
     </main>
